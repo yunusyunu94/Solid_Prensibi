d675390b7f691860eec5871df0e5f3ff012d1374
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-23 21:54:17 +0300

    Liskov_Substitution_Princilbe_Ornek2

---
+++ Solid_Prensibi/Single_Responsibility_Prensibi/Ornek2/EmployeeConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Prensibi.Single_Responsibility_Prensibi.Ornek2
+{
+    public class EmployeeConnection
+    {
+        public static SqlConnection connec = new SqlConnection("Server=localhost\\SQLEXPRESS; " +
+        "Database=Northwind; Trusted_Connection=True;\" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities -ContextDir Contexts\r\n ");
+
+    }
+}
