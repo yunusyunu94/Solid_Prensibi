e9d8e5010d300331ed73cd4f1ef583d4ae1f2a27
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-25 21:32:37 +0300

    Open_Closed_Principle_Ornek2

---
+++ Solid_Prensibi/Open_Closed_Principle/Ornek2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Prensibi.Open_Closed_Principle.Ornek2
+{
+    public class Program
+    {
+        static void Mail(string[] strings)
+        {
+            CostomerManeger df = new(new BirinciMevzuat());
+            df.add();
+        }
+    }
+
+
+    public class CostomerManeger
+    {
+        private readonly IMevzuat _mevzuat;
+
+        public CostomerManeger(IMevzuat mevzuat)
+        {
+            this._mevzuat = mevzuat;
+            mevzuat.IslemYap();
+        }
+        public void add()
+        {
+            _mevzuat.IslemYap();
+        }
+    }
+
+    public interface IMevzuat
+    {
+        void IslemYap();
+    }
+
+    public class BirinciMevzuat : IMevzuat
+    {
+        public void IslemYap()
+        {
+            Console.WriteLine("BirinciMevzuat");
+        }
+    }
+
+    public class İkinciMevzuat : IMevzuat
+    {
+        public void IslemYap()
+        {
+            Console.WriteLine("İkinciMevzuat");
+        }
+    }
+}
