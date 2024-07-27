a776f5269e57387fd38a76617838a8bdc9bab6e3
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-27 11:54:47 +0300

    Open_Closed_Principle_Ornek_3

---
+++ Solid_Prensibi/Open_Closed_Principle/Ornek3/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle._3.Örnek
+{
+    public class Circle : Shape 
+    {
+        
+        public double Radius { get; set; } 
+
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+
+        public override double Area() 
+        {
+            return Radius * Radius * Math.PI;
+        }
+    }
+}
