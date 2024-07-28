c747923724b633b5c0e9e558c128af95386195b1
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-28 21:33:38 +0300

    Open_Closed_Principle_Ornek4

---
+++ Solid_Prensibi/Open_Closed_Principle/Ornek4/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle._4.Ornek
+{
+    public class TestClass
+    {
+        public void CalculateTripCost()
+        {
+            var calculator = new FuelCostCalculator();
+
+            var cost = calculator.Calculater(new MercedesBenz());
+
+            Console.WriteLine($"Total cost for this car is {cost} TL");
+
+
+            // Veya
+
+            var calculator2 = new FuelCostCalculator();
+            var cost2 = calculator.Calculater(new BMW());
+
+            Console.WriteLine($"Total cost for this car is {cost} TL");
+
+            FuelCostCalculator ef = new FuelCostCalculator();
+
+            Console.WriteLine($"Total cost for this car is  {ef.Calculater(new BMW())} TL");
+        }
+    }
+}
