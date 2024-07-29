feda757d2064804de5c75da2065f34f7d4f45a9d
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-07-29 22:14:40 +0300

    Liskov_Substitution_Princilbe_Ornek1

---
+++ Solid_Prensibi/Liskov_Substitution_Princilbe/Ornek1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+#region İdeal Olmayan Code
+//using static Liskov_Substitution_Princilbe.İdeal_Olmayan_Code;
+
+//Clound clound = new AWS();
+
+//clound.MachineLearning();
+//clound.Translate();
+
+//clound = new Google();
+
+//clound.MachineLearning();
+//clound.Translate();
+
+//clound = new Azure();
+
+//clound.MachineLearning();
+//clound.Translate();         // Buradaki nesnede cikti vermeyecektir.
+
+#endregion
+
+#region İdeal Olmayan Code
+using static Liskov_Substitution_Princilbe.Ornek1.İdeal_Code;
+
+Clound clound = new AWS();
+clound.MachineLearning();
+(clound as ITranslatable)?.Translate();
+
+clound = new Google();
+clound.MachineLearning();
+(clound as ITranslatable)?.Translate();
+
+clound = new Azure();
+clound.MachineLearning();
+(clound as ITranslatable)?.Translate();
+
+Clound dene = new AWS();
+dene.MachineLearning();
+
+
+#endregion
+
---
+++ Solid_Prensibi/Liskov_Substitution_Princilbe/Ornek1/İdeal_Code.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov_Substitution_Princilbe.Ornek1
+{
+
+
+    // Link : https://www.youtube.com/watch?v=URpeK0eiANU&ab_channel=Gen%C3%A7ayY%C4%B1ld%C4%B1z
+
+
+
+
+    public class İdeal_Code
+    {
+        public abstract class Clound
+        {
+            public abstract void MachineLearning();
+        }
+
+        public interface ITranslatable
+        {
+            void Translate();
+        }
+
+
+
+        public class AWS : Clound, ITranslatable
+        {
+
+            public void Translate()
+            => Console.WriteLine("AWS Translate");
+
+            public override void MachineLearning()
+            => Console.WriteLine("AWS Machine Learning");
+
+        }
+
+        public class Azure : Clound
+        {
+
+            public override void MachineLearning()
+            => Console.WriteLine("Azure Machine Learning");
+
+        }
+
+        public class Google : Clound, ITranslatable
+        {
+            public void Translate()
+            => Console.WriteLine("Google Translate");
+
+            public override void MachineLearning()
+            => Console.WriteLine("Google Machine Learning");
+
+
+        }
+    }
+}
