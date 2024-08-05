7bd53005f919c196f13ec83801cc6c8a46402112
k-yunus <112016542+k-yunus@users.noreply.github.com>
2024-08-05 21:02:22 +0300

    Dependency_Inversion_Principle_Ornek1

---
+++ Solid_Prensibi/Dependency_Inversion_Principle/Ornek1/İdeal_Olmayan_Code.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion_Principle.Ornek1
+{
+    public class İdeal_Olmayan_Code
+    {
+        public class MailService
+        {
+            public void SendMail(Gmail gmail)
+            {
+                gmail.Send("...");
+
+
+            }
+        }
+
+        public class Gmail
+        {
+            public void Send(string mail)
+            {
+                // .. Sned Mail ..
+            }
+        }
+        class Yandex
+        {
+            public void SendMail(string mail, string to)
+            {
+                // ..Sned Mail..
+            }
+        }
+        class Hotmail
+        {
+            public void Send(string mail)
+            {
+                // ..Sned Mail..
+            }
+        }
+
+    }
+}
